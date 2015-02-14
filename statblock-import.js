998b847623a537b1a041a418bfffbbb3245fd0a1
Jean-Francois Rondeau <jean_f_rondeau@hotmail.com>
2015-02-13 21:48:20 -0500

    Fonctionnel
    Pas testé action legendaire

--- statblock-import.js
+++ statblock-import.js
@@ -66,6 +66,35 @@ on("chat:message", function(msg) {
     log("================");
 });
 
+function splitRegex(source, regex, map)
+{
+    var precedentKey;
+    var precedentIndex = 0;
+    var debut = 0;
+    var section = {};
+
+    while(match = regex.exec(source))
+    {
+        if(match.index > 0 && precedentKey != undefined)
+        {
+            if(map)
+                section[precedentKey] = map(source.substring(precedentIndex + precedentKey.length + 2, match.index).trim());    
+            else
+                section[precedentKey] = source.substring(precedentIndex + precedentKey.length + 2, match.index).trim();
+        }
+            
+        precedentKey = match[1].toLowerCase();
+        precedentIndex = match.index;
+
+        if(debut == 0 ) {
+            debut = precedentIndex;
+            section.debut = match.index;
+        }
+    }    
+    section[precedentKey] = source.substring(precedentIndex + precedentKey.length + 2).trim();
+    return section;
+}
+
 
 var Parser = function() {
     this.keyword = ['armor class', 'hit points', 'speed', 'str', 'dex', 'con', 'int', 'wis', 'cha', 'saving throws', 'skills', 'damage resistance', 'damage immunities', 'condition immunities','damage vulnerabilities','senses', 'languages', 'challenge', 'traits', 'actions','legendary actions', 'bio'];
@@ -76,43 +105,34 @@ var Parser = function() {
         
         this.statblock = this.clean(statblock);
         section = this.splitSection(this.statblock);
+
         this.createCharacter(section.title);
-        
-        var keys = Object.keys(section);
-        keys = sortString(keys); // Be sure to process abilities first since other attribut need abilities modifier to be computed.
-        
-        for(i = 0, len = keys.length; i < len;++i)
-        {    
-            //log("Len=" + len + "; i = " + i);
-            key = keys[i];
-            //log('Parsing ' + key);
+
+        // Be sure to process abilities first since other attribut need abilities modifier to be computed.
+        this.parseAbilities(section['abilities']);
+
+        _.each(section, function(value, key)
+        {
             switch (key) {
-                case 'armor class':
-                    this.parseArmorClass(section[key]); break;
-                case 'size':
-                    this.parseSize(section[key]); break;
-                case 'hit points':
-                    this.parseHp(section[key]); break;
-                case 'speed':
-                    this.parseSpeed(section[key]); break;
-                case 'abilities':
-                    this.parseAbilities(section[key]); break;
-                case 'challenge':
-                    this.parseChallenge(section[key]); break;
-                case 'saving throws':
-                    this.parseSavingThrow(section[key]); break;
-                case 'skills':
-                    this.parseSkills(section[key]); break;
+                case 'armor class': this.parseArmorClass(section[key]); break;
+                case 'size': this.parseSize(section[key]); break;
+                case 'hit points': this.parseHp(section[key]); break;
+                case 'speed': this.parseSpeed(section[key]); break;
                 
-                case 'damage resistance':
+                case 'challenge': this.parseChallenge(section[key]); break;
+                case 'saving throws': this.parseSavingThrow(section[key]); break;
+                case 'skills': this.parseSkills(section[key]); break;
+                case 'traits': this.parseTraits(section[key]); break;
+                case 'actions': this.parseActions(section[key]); break;
+                case 'damage resistances':
                 case 'damage immunities':
                 case 'condition immunities':
                 case 'damage vulnerabilities':
                 case 'senses':
                 case 'languages':
-                    var attr = 'npc_' + key.replace(' ', '_').replace('ties', 'ty');
-                    var value = section[key].substring(key.length+1);
-                    this.setAttribut(this.npc.id, attr, value);
+
+                    var attr = 'npc_' + key.replace(' ', '_').replace('ties', 'ty');                    
+                    this.setAttribut(this.npc.id, attr, section[key]);
                     break;
                 
                 case 'title': 
@@ -121,7 +141,9 @@ var Parser = function() {
                 default:
                     log("Missing parse " + key)
             }
-        }
+        }, this);
+
+        
     }
 
 
@@ -154,39 +176,29 @@ var Parser = function() {
     },
 
     this.splitSection = function(statblock) {
-        var keyPos = {};
-        var regex = /#\s*(armor class|hit points|speed|str|dex|con|int|wis|cha|saving throws|skills|damage resistance|damage immunities|condition immunities|damage vulnerabilities|senses|languages|challenge|traits|actions|legendary actions|bio)(?:\s|#)/gmi;
-        //log(statblock);
-        while(match = regex.exec(statblock)){
-            keyPos[match[1].toLowerCase()] = match.index;
-        };
-        
+
+        var attributes = statblock;
+        var powers;
+
         // Cherche l'index du début du trait
-        var match = statblock.match(/#(?!armor class|hit points|speed|str|dex|con|int|wis|cha|saving throws|skills|damage resistance|damage immunities|condition immunities|damage vulnerabilities|senses|languages|challenge|traits|actions|legendary actions|bio)((?:\w|\s)+)\./i);
-        keyPos['traits'] = match.index;
-        
-        var keysSorted = Object.keys(keyPos).sort(function(a, b) {
-            return keyPos[b] - keyPos[a]
-        })
-        
-        var last = statblock.length;
-        var section = {};
-                
-        _.each(keysSorted, function(key){
-            var content = statblock.substring(keyPos[key], last);
-            if(content.indexOf('#' + key)===0)
-            {
-                content = content.substring(key.length + 1);
-            }
-            if(key != 'traits' && key != 'actions' && key != 'legendary actions')
-                content = content.replace(/#/g, ' ');
-            section[key] = content.trim();
-            last = keyPos[key];
-        })
-        
+        var match = statblock.match(/#(?!armor class|hit points|speed|str|dex|con|int|wis|cha|saving throws|skills|damage resistances|damage immunities|condition immunities|damage vulnerabilities|senses|languages|challenge|traits|actions|legendary actions|bio)((?:\w|\s)+)\./i);
+        var posTraits = match.index;
+        if(match !== null)
+        {
+            attributes = statblock.substring(0, match.index);
+            powers = statblock.substring(match.index);
+        }    
+
+        // Split les attributs du statblock;
+        var regex = /#\s*(armor class|hit points|speed|str|dex|con|int|wis|cha|saving throws|skills|damage resistances|damage immunities|condition immunities|damage vulnerabilities|senses|languages|challenge)(?:\s|#)/gmi;
+        var section = splitRegex(attributes, regex, function(texte){
+            return texte.replace(/\.#/g,'.\n').replace(/#/g,' ');  
+        });
+
+        // Traite title, size and abilities
         var pos = statblock.indexOf('#');
         section['title'] =  statblock.substring(0, pos);
-        section['size'] = statblock.substring(pos+1, last);
+        section['size'] = statblock.substring(pos+1, section.debut);
         
         var abilitiesName = ['str', 'dex', 'con', 'int', 'wis', 'cha'];
         var abilities = '';
@@ -197,6 +209,14 @@ var Parser = function() {
             }
         }
         section['abilities'] = abilities;
+
+        // Traite le reste du statblock
+        var section2 = splitRegex(powers, /#\s*(actions|legendary actions|bio)(?:\s|#)/gmi);
+        section2['traits'] = powers.substring(0, section2.debut);
+        
+        for (var attrname in section2) { section[attrname] = section2[attrname]; } // Combine le tout.
+        delete section.debut;
+
         return section;
     }
 
@@ -217,9 +237,7 @@ var Parser = function() {
             log("Character will be updated");
         }
 
-        obj.set({
-            gmnotes: this.statblock.replace(/#/g , "\n")
-        });
+        obj.set({gmnotes: this.statblock.replace(/#/g , "\n")});
         this.npc = obj;
         this.setAttribut(obj.id, 'is_npc', 1);
     }
@@ -358,17 +376,13 @@ var Parser = function() {
     this.parseActions = function(input){
         
         var actions = this.parsePower(input);
-        
-        /*npc_action_name1
-        npc_action_description1
-        npc_action_effect1
-        */
-        
         var cpt = 1;
         
          _.each(actions, function(value, key){
             this.setAttribut(this.npc.id, 'npc_action_name' + cpt, key);
-            this.setAttribut(this.npc.id, 'npc_action_description' + cpt, value);
+            var pos = value.indexOf('Hit:');
+            this.setAttribut(this.npc.id, 'npc_action_description' + cpt, value.substring(0, pos).trim());
+            this.setAttribut(this.npc.id, 'npc_action_effect' + cpt, value.substring(pos).trim());
             cpt++;
         }, this);
     }
@@ -393,3 +407,32 @@ var Parser = function() {
         return power;
     }
 };
+
+
+
+var obj = {
+    id: 1,
+    set: function(attr) {
+        for (var prop in attr) {
+            if (attr.hasOwnProperty(prop)) {
+                this[prop] = attr[prop];
+            }
+        }
+    }
+}
+function on(event, callback){}
+function findObjs(filter){ return [{id: 1}]}
+function getObj(id){ return obj;}
+function getAttrByName(id,attr) {return obj[attr];}
+function log(message) {console.log(message);}
+
+_ = require('./underscore-min.js');
+
+
+statblock = "Test%20BUGBEAR%3Cbr%3EMedium%20humanoid%20%28goblinoid%29%2C%20chaotic%20evil%3Cbr%3EArmor%20Class%2016%20%28hide%20armor%2C%20shield%29%3Cbr%3EHit%20Points%2027%20%285d8%20+%205%29%3Cbr%3ESpeed%2040%20ft.%2C%20climb%2040%20ft.%2C%20fly%2080%20ft.%3Cbr%3E%20%20STR%20%20DEX%20%20CON%20%20INT%20%20WIS%20%20CHA%3Cbr%3E%20%2015%20%28+2%29%20%2014%20%28+2%29%20%2013%20%28+1%29%20%208%20%28%u22121%29%20%2011%20%28+0%29%20%209%20%28%u22121%29%3Cbr%3ESaving%20Throws%20Wis%20+2%2C%20Cha%20+4%3Cbr%3ESkills%20Stealth%20+6%2C%20Survival%20+2%2C%20Athletics%20+5%2C%20Perception%20+3%3Cbr%3EDamage%20Resistances%20acid%2C%20fire%2C%20lightning%2C%20thunder%3B%20bludgeoning%2C%20%3Cbr%3Epiercing%2C%20and%20slashing%20from%20nonmagical%20weapons%3Cbr%3EDamage%20Immunities%20cold%2C%20necrotic%2C%20poison%3Cbr%3ECondition%20Immunities%20charmed%2C%20exhaustion%2C%20frightened%2C%20%3Cbr%3Egrappled%2C%20paralyzed%2C%20petrified%2C%20poisoned%2C%20prone%2C%20restrained%3Cbr%3EDamage%20Vulnerabilities%20fire%3Cbr%3ESenses%20darkvision%2060%20ft.%2C%20passive%20Perception%2010%3Cbr%3ELanguages%20Common%2C%20Goblin%3Cbr%3EChallenge%201%20%281%2C200%20XP%29%3Cbr%3EBrute.%20A%20melee%20weapon%20deals%20one%20extra%20die%20of%20its%20damage%20when%20the%20%3Cbr%3Ebugbear%20hits%20with%20it%20%28included%20in%20the%20attack%29.%3Cbr%3ESurprise%20Attack.%20If%20the%20bugbear%20surprises%20a%20creature%20and%20hits%20it%20%3Cbr%3Ewith%20an%20attack%20during%20the%20first%20round%20of%20combat%2C%20the%20target%20takes%20%3Cbr%3Ean%20extra%207%20%282d6%29%20damage%20from%20the%20attack.%3Cbr%3EActions%3Cbr%3EMorningstar.%20Melee%20Weapon%20Attack%3A%20+4%20to%20hit%2C%20reach%205%20ft.%2C%20one%20%3Cbr%3Etarget.%20Hit%3A%2011%20%282d8%20+%202%29%20piercing%20damage.%3Cbr%3EJavelin.%20Melee%20or%20Ranged%20Weapon%20Attack%3A%20+4%20to%20hit%2C%20reach%205%20ft.%20or%20%3Cbr%3Erange%2030/120%20ft.%2C%20one%20target.%20Hit%3A%209%20%282d6%20+%202%29%20piercing%20damage%20%3Cbr%3Ein%20melee%20or%205%20%281d6%20+%202%29%20piercing%20damage%20at%20range.%3Cbr%3Ebio%3Cbr%3EBugbears%20are%20hairy%20goblinoids%20born%20for%20battle%20and%20%3Cbr%3Emayhem.%20They%20survive%20by%20raiding%20and%20hunting%2C%20but%20are%20%3Cbr%3Efond%20of%20setting%20ambushes%20and%20fleeing%20when%20outmatched.%3Cbr%3E%3Cbr%3E";
+
+parser = new Parser();
+parser.setAttribut = function(id, name, currentVal, max) {obj[name] = currentVal; return;}  // Override for test
+parser.parse(statblock);
+
+log(obj);
