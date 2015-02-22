add9b3c8cfea4de0e444b97040e692c072ae8a4c
Jean-Francois Rondeau <jean_f_rondeau@hotmail.com>
2015-02-21 20:41:22 -0500

    Update statblock-import
    Add error logging. More robust. Patch bad skill spelling. Monster now
    local to function. Rename parse by ImportStatblock.

--- statblock-import-5e.js
+++ statblock-import-5e.js
@@ -1,9 +1,5 @@
 (function (jf, undefined){
-    
-    var status = 'created';
-
     jf.whisperTraitsToGm = true;
-
     jf.statblock = {
         version: 1,
         RegisterHandlers: function () {
@@ -11,119 +7,9 @@
             log("JF Statblock ready");
         }
     }
-
-    jf.capitalizeEachWord = function(str) {
-        return str.replace(/\w\S*/g, function(txt) {
-            return txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase();
-        });
-    };
-
-    jf.parse = function(statblock){
-
-        //Clean text       
-        statblock = unescape(statblock).replace(/<br[^>]*>/g, '#').replace(/(<([^>]+)>)/ig,"").replace('–', '-');
-
-        section = splitSection(statblock);
-        var statut = 'created';
-
-        monster = jf.getCharacter(section.title, statblock.replace(/#/g, '<br>'), section.bio);
-
-        if('abilities' in section) parseAbilities(); // Be sure to process abilities first since other attributes need abilities modifier to be know.
-        if('armor class' in section) parseArmorClass();
-        if('size' in section) parseSize(); 
-        if('hit points' in section) parseHp();
-        if('speed' in section) parseSpeed();
-        if('challenge' in section) parseChallenge();
-        if('saving throws' in section) parseSavingThrow();
-        if('skills' in section) parseSkills();
-        
-        if('traits' in section) parseTraits();
-        if('actions' in section) parseActions();
-
-        if('damage immunities' in section) jf.setAttribut(monster.id, 'npc_damage_immunity', section['damage immunities']);
-        if('condition immunities' in section) jf.setAttribut(monster.id, 'npc_condition_immunity', section['condition immunities']);
-        if('damage vulnerabilities' in section) jf.setAttribut(monster.id, 'npc_damage_vulnerability', section['damage vulnerabilities']);
-        if('senses' in section) jf.setAttribut(monster.id, 'npc_senses', section['senses']);
-        if('languages' in section) jf.setAttribut(monster.id, 'npc_languages', section['languages']);
-        if('damage resistances' in section) jf.setAttribut(monster.id, 'npc_damage_resistance', section['damage resistances']);
-        
-        
-        message = 'Character ' + status;
-        log(message);
-        sendChat('GM', message);
     
-        return monster;
-    }
-
-    jf.getCharacter = function(name, gmnotes, bio) {
-
-        name = jf.capitalizeEachWord(name);
-
-        var obj = findObjs({_type: "character", name: name});
-
-        if (obj.length == 0) {
-            obj = jf.fixedCreateObj('character', {
-                name: name
-            });
-        } else {
-            obj = getObj('character', obj[0].id);
-            status = 'updated';
-        }
-
-        if(gmnotes != undefined)
-            obj.set({gmnotes: gmnotes});
-
-        if(bio != undefined)
-            obj.set({bio: bio});
-        
-        jf.setAttribut(obj.id, 'is_npc', 1);
-        return obj;
-    };
-
-    jf.setAttribut = function(id, name, currentVal, max) {
-        max = max || '';
-        
-        if(currentVal == undefined){
-            log("Error setting empty value: " + name);
-            return;
-        }
-
-        var attr = findObjs({
-            _type: 'attribute',
-            _characterid: id,
-            name: name
-        })[0];
-
-        
-        if (attr == undefined) {
-            createObj('attribute', {
-                name: name,
-                current: currentVal,
-                max: max,
-                characterid: id
-            });
-        } 
-        else {//log("Trying to set " + name);
-            attr.set({
-                current: currentVal,
-                max: max
-            });
-        }
-    };
-
-    jf.fixedCreateObj = jf.fixedCreateObj || function() {
-        var obj = createObj.apply(this, arguments);
-        if (obj && !obj.fbpath) {
-            obj.fbpath = obj.changed._fbpath.replace(/([^\/]*\/){4}/, "/");
-        }
-        return obj;
-    };
-
-    jf.RegisterEventHandlers = function(){
-        
-        
-    }
-
+    var status = '';
+    
     function HandleInput(msg){
         if (msg.type !== "api") {
             return;
@@ -142,10 +28,145 @@
             sendChat("GM", "No token selected");
             return;
         }
+        
+        jf.ImportStatblock(token);
+    }
+    
 
-        return jf.parse(token.get('gmnotes'));
+    jf.capitalizeEachWord = function(str) {
+        return str.replace(/\w\S*/g, function(txt) {
+            return txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase();
+        });
     }
 
+    jf.getCharacter = function(name, gmnotes, bio) {
+        if(name == undefined)
+            throw("Impossible to create or update character if name not found");
+        
+        name = jf.capitalizeEachWord(name);
+
+        var obj = findObjs({_type: "character", name: name});
+
+        if (obj.length == 0) {
+            obj = jf.fixedCreateObj('character', {
+                name: name
+            });
+            status = 'Character ' + name + ' created';
+        } else {
+            obj = getObj('character', obj[0].id);
+            status = 'Character ' + name + ' updated';
+        }
+        
+        if(obj == undefined)
+            throw("Something prevent script to create or find character " + name);
+
+        if(gmnotes != undefined)
+            obj.set({gmnotes: gmnotes});
+
+        if(bio != undefined)
+            obj.set({bio: bio});
+        
+        jf.setAttribut(obj.id, 'is_npc', 1);
+        return obj;
+    }
+
+    jf.setAttribut = function(id, name, currentVal, max) {
+        
+        if(name == undefined)
+            throw("Attribut non défini pour la valeur " + currentVal);
+        
+        max = max || '';
+        
+        if(currentVal == undefined){
+            log("Error setting empty value: " + name);
+            return;
+        }
+
+        var attr = findObjs({
+            _type: 'attribute',
+            _characterid: id,
+            name: name
+        })[0];
+
+        
+        if (attr == undefined) {
+            log("Creating attribut " + name);
+            createObj('attribute', {
+                name: name,
+                current: currentVal,
+                max: max,
+                characterid: id
+            });
+        } 
+        else {//log("Trying to set " + name);
+            if(attr.current != undefined && attr.current.toString() != currentVal) {
+                log("Updating attribut " + name);
+                attr.set({current: currentVal, max: max});
+            }
+        }
+    }
+
+    jf.fixedCreateObj = jf.fixedCreateObj || function() {
+        var obj = createObj.apply(this, arguments);
+        if (obj && !obj.fbpath) {
+            obj.fbpath = obj.changed._fbpath.replace(/([^\/]*\/){4}/, "/");
+        }
+        return obj;
+    }
+
+    jf.ImportStatblock = function(token){
+        log("Parsing statblock...");
+        
+        var errors = [];
+        status = 'Nothing modified';
+        
+        try {    
+            var statblock = token.get('gmnotes').trim();
+            
+            if(statblock == '')
+                throw("This token GM Notes was empty.");
+           
+            //Clean text       
+            statblock = unescape(statblock).trim().replace(/<br[^>]*>/g, '#').replace(/(<([^>]+)>)/ig,"").replace('–', '-');
+            section = splitSection(statblock);
+                        
+            var monster = jf.getCharacter(section.title, statblock.replace(/#/g, '<br>'), section.bio);
+            var id = monster.id;
+            
+            if('abilities' in section) parseAbilities(id); // Be sure to process abilities first since other attributes need abilities modifier to be know.
+            if('armor class' in section) parseArmorClass(id);
+            if('size' in section) parseSize(id); 
+            if('hit points' in section) parseHp(id);
+            if('speed' in section) parseSpeed(id);
+            if('challenge' in section) parseChallenge(id);
+            if('saving throws' in section) parseSavingThrow(id);
+            if('skills' in section) parseSkills(id);
+            
+            if('traits' in section) parseTraits(id);
+            if('actions' in section) parseActions(id);
+    
+            if('damage immunities' in section) jf.setAttribut(id, 'npc_damage_immunity', section['damage immunities']);
+            if('condition immunities' in section) jf.setAttribut(id, 'npc_condition_immunity', section['condition immunities']);
+            if('damage vulnerabilities' in section) jf.setAttribut(id, 'npc_damage_vulnerability', section['damage vulnerabilities']);
+            if('senses' in section) jf.setAttribut(id, 'npc_senses', section['senses']);
+            if('languages' in section) jf.setAttribut(id, 'npc_languages', section['languages']);
+            if('damage resistances' in section) jf.setAttribut(id, 'npc_damage_resistance', section['damage resistances']);
+        
+        } 
+        catch (e) {
+            status = "Parsing was incomplete due to error(s)";
+            errors.push(e);
+        }
+        
+        log(status);
+        sendChat('GM', '/w GM '+ status);
+        
+        if(errors.length > 0)
+        {
+            log(errors);    
+            sendChat('GM', '/w GM Error(s):\n/w GM ' + errors.join('\n/w GM '));
+        }
+    }
 
     function splitRegex(source, regex){
         var precedentKey = 'debut';
@@ -164,7 +185,7 @@
         if(section.debut == '') delete section.debut;
 
         return section;
-    };
+    }
 
     function splitSection(statblock){
         var sectionTraits;
@@ -242,43 +263,42 @@
             }
         }
         return section;
-    };
+    }
 
-
-    function parseAbilities(){
+    function parseAbilities(id){
         var regex = /(\d+)\s*\(/g;
         var match = [];
         while(matches = regex.exec(section['abilities'])){
             match.push(matches[1]);
         }
-        jf.setAttribut(monster.id, 'npc_strength', match[0]);
-        jf.setAttribut(monster.id, 'npc_dexterity', match[1]);
-        jf.setAttribut(monster.id, 'npc_constitution', match[2]);
-        jf.setAttribut(monster.id, 'npc_intelligence', match[3]);
-        jf.setAttribut(monster.id, 'npc_wisdom', match[4]);
-        jf.setAttribut(monster.id, 'npc_charisma', match[5]);
+        jf.setAttribut(id, 'npc_strength', match[0]);
+        jf.setAttribut(id, 'npc_dexterity', match[1]);
+        jf.setAttribut(id, 'npc_constitution', match[2]);
+        jf.setAttribut(id, 'npc_intelligence', match[3]);
+        jf.setAttribut(id, 'npc_wisdom', match[4]);
+        jf.setAttribut(id, 'npc_charisma', match[5]);
     }
 
-    function parseArmorClass(){
+    function parseArmorClass(id){
         var match = section['armor class'].match(/(\d+)\s?(.*)/);
-        jf.setAttribut(monster.id, 'npc_AC', match[1]);
-        jf.setAttribut(monster.id, 'npc_AC_note', match[2]);
+        jf.setAttribut(id, 'npc_AC', match[1]);
+        jf.setAttribut(id, 'npc_AC_note', match[2]);
     }
 
-    function parseSize() {
+    function parseSize(id){
         var match = section['size'].match(/(.*?) (.*?), (.*)/i);
-        jf.setAttribut(monster.id, 'npc_size', match[1]);
-        jf.setAttribut(monster.id, 'npc_type', match[2]);
-        jf.setAttribut(monster.id, 'npc_alignment', match[3]);
+        jf.setAttribut(id, 'npc_size', match[1]);
+        jf.setAttribut(id, 'npc_type', match[2]);
+        jf.setAttribut(id, 'npc_alignment', match[3]);
     }
 
-    function parseHp() {
+    function parseHp(id){
         var match = section['hit points'].match(/.*?(\d+)\s+\(((?:\d+)d(?:\d+))/i);
-        jf.setAttribut(monster.id, 'npc_HP', match[1], match[1]);
-        jf.setAttribut(monster.id, 'npc_HP_hit_dice', match[2]);
+        jf.setAttribut(id, 'npc_HP', match[1], match[1]);
+        jf.setAttribut(id, 'npc_HP_hit_dice', match[2]);
     }
     
-    function parseSpeed(){
+    function parseSpeed(id){
         var baseAttr = 'npc_speed';
         var regex = /(|fly|climb|swim|burrow)\s*(\d+)(?:ft\.|\s)+(\(.*\))?/gi;
         while(match = regex.exec(section['speed']))
@@ -289,18 +309,18 @@
             if(match[3] != undefined)
                 value += ' ' + match[3];
 
-            jf.setAttribut(monster.id, attrName, value);
+            jf.setAttribut(id, attrName, value);
         }
     }
 
-    function parseChallenge(){
+    function parseChallenge(id){
         input = section['challenge'].replace(/[, ]/g, '');
         var match = input.match(/([\d/]+).*?(\d+)/);
-        jf.setAttribut(monster.id, 'npc_challenge', match[1]);
-        jf.setAttribut(monster.id, 'npc_xp', parseInt(match[2]));
+        jf.setAttribut(id, 'npc_challenge', match[1]);
+        jf.setAttribut(id, 'npc_xp', parseInt(match[2]));
     }
 
-    function parseSavingThrow(){
+    function parseSavingThrow(id){
         var regex = /(STR|DEX|CON|INT|WIS|CHA).*?(\d+)/gi;
         var attr, value;
         while(match = regex.exec(section['saving throws'])){            
@@ -313,25 +333,28 @@
                 case 'wis': attr = 'npc_wisdom'; break;
                 case 'cha': attr = 'npc_charisma'; break;
             }
-            jf.setAttribut(monster.id, attr + '_save_bonus', match[2] - Math.floor((getAttrByName(monster.id,attr)-10)/2));
+            jf.setAttribut(id, attr + '_save_bonus', match[2] - Math.floor((getAttrByName(id,attr)-10)/2));
         }
     }
 
-    function parseSkills(){
-        var skills = {acrobatics: 'dexterity', "animal handling": 'wisdom', arcana: 'intelligence', athletics: 'strength', deception: 'charisma',history: 'intelligence',insigth: 'wisdom',intimidation: 'charisma',investigation: 'intelligence',medecine: 'wisdom',nature: 'intelligence',perception: 'wisdom',performance: 'charisma',persuation: 'charisma',religion: 'intelligence',"sleight of Hand": 'dexterity', stealth: 'dexterity', survival: 'wisdom'};
+    function parseSkills(id){
+        var skills = {acrobatics: 'dexterity', "animal handling": 'wisdom', arcana: 'intelligence', athletics: 'strength', deception: 'charisma',history: 'intelligence',insigth: 'wisdom',intimidation: 'charisma',investigation: 'intelligence',medicine: 'wisdom',nature: 'intelligence',perception: 'wisdom',performance: 'charisma',persuation: 'charisma',religion: 'intelligence',"sleight of Hand": 'dexterity', stealth: 'dexterity', survival: 'wisdom'};
         input = section['skills'].replace(/Skills\s+/i, '');
         var regex = /(\w+).*?(\d+)/gi;
         while(match = regex.exec(input)){
             var skill = match[1].toLowerCase();
-            var abilitymod = skills[skill];
-            var attr = 'npc_' + skill.replace(' ','') + '_bonus';
-            //var tmp = Math.floor((getAttrByName(monster.id,'npc_'+ abilitymod)-10)/2);
-            //log(abilitymod + " = " + tmp);
-            jf.setAttribut(monster.id, attr, match[2] - Math.floor((getAttrByName(monster.id,'npc_'+ abilitymod)-10)/2));
+            if(skill in skills) {
+                var abilitymod = skills[skill];
+                var attr = 'npc_' + skill.replace(' ','') + '_bonus';
+                jf.setAttribut(id, attr, match[2] - Math.floor((getAttrByName(id,'npc_'+ abilitymod)-10)/2));
+            }
+            else {
+                errors.push("Skill " + skill + ' is not a valid skill');
+            }
         }
     }
 
-    function parseTraits(){
+    function parseTraits(id){
         var texte = "";
         var traits = splitRegex(section['traits'], /#([A-Z].*?)\./g);
 
@@ -357,13 +380,13 @@
         if(jf.whisperTraitsToGm == true)
             texte = texte.replace(/\n/g, '\n/w GM ');
 
-        jf.setAttribut(monster.id, 'npc_traits', texte);
+        jf.setAttribut(id, 'npc_traits', texte);
     }
 
-    function parseActions(){
+    function parseActions(id){
         if(section.actions.indexOf('#')!==0)
             section.actions = '#' + section.actions;
-
+        
         var actions = splitRegex(section['actions'], /#((?:[A-Z][a-z]+[0-9-\s(\/)]{0,5})+)\./g);
         var cpt = 1;
 
@@ -372,29 +395,28 @@
             if(value  == '') return;
 
             if(key == 'multiattack') {
-                jf.setAttribut(monster.id, 'npc_multiattack', value.replace(/\.#/g,'.\n').replace(/#/g,' '));
+                jf.setAttribut(id, 'npc_multiattack', value.replace(/\.#/g,'.\n').replace(/#/g,' '));
                 return;
             }
+            //log(key + ': ' + value);
 
-            jf.setAttribut(monster.id, 'npc_action_name' + cpt, jf.capitalizeEachWord(key));
-            //var pos = value.indexOf('Hit:');
+            jf.setAttribut(id, 'npc_action_name' + cpt, jf.capitalizeEachWord(key));
+            
             var match = value.match(/(Each|Hit:)/);
             if(match != null) {
                 texte = value.substring(0, match.index).replace(/\.#/g,'.\n').replace(/#/g,'').trim();
                 texte = texte.replace(/(\+(\d+))/g, '$1 : [[1d20+$2]]|[[1d20+$2]]');
-                jf.setAttribut(monster.id, 'npc_action_description' + cpt, texte);
+                jf.setAttribut(id, 'npc_action_description' + cpt, texte);
 
                 texte = value.substring(match.index).replace(/\.#/g,'.\n').replace(/#/g,'').trim();
                 texte = texte.replace(/(\d+d\d+[\d\s+]*)/g, '[[$1]]')
-                jf.setAttribut(monster.id, 'npc_action_effect' + cpt, texte);
-
+                jf.setAttribut(id, 'npc_action_effect' + cpt, texte);
             }
             else {
                 texte = value.replace(/\.#/g,'.\n').replace(/#/g,'').trim();
                 texte = texte.replace(/(\+(\d+))/g, '$1 : [[1d20+$2]]|[[1d20+$2]]');
-                jf.setAttribut(monster.id, 'npc_action_description' + cpt, texte);
+                jf.setAttribut(id, 'npc_action_description' + cpt, texte);
             }
-            
             cpt++;
         }, this);
     }   
@@ -403,9 +425,5 @@
 
 on("ready",function(){
     'use strict';
-
     jf.statblock.RegisterHandlers();
 });
-
-
-//jf.whisperTraitsToGm = false; //Default to true. If true, the traits information will be prefixed with /w GM so only GM will see Traits in chat windows.
